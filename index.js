34423a513384050af2ec4953f79be8e2c83850ce
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-31 16:21:41 -0700

    game level complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(e){
   let codeSubmit = document.querySelector('#codeSubmit')
   let lineOfCode = document.querySelector('#lineOfCode')
+  let victoryText = document.querySelector('.victoryText')
 
   let ferretLand = document.querySelector('.ferretLand')
+
 
   function testFerretLand(codeToTest){
     let propertyName = codeToTest.slice(0, codeToTest.indexOf(":"))
@@ -13,6 +15,30 @@ document.addEventListener('DOMContentLoaded', function(e){
     ferretLand.style.setProperty(propertyName, propertyValue)
   }
 
+  function checkForVictory(){
+    // justify-content: center;
+    // align-items: center;
+    // flex-direction: column;
+    let currentStyles = ferretLand.getAttribute('style')
+
+    if(currentStyles.includes('justify-content: center;')) {
+      if(currentStyles.includes('align-items: center;')) {
+        if(currentStyles.includes('flex-direction: column;')) {
+          // victory!
+          victoryText.style.display = 'block'
+
+        } else{
+          victoryText.style.display = 'none'
+        }
+      } else {
+        victoryText.style.display = 'none'
+      }
+    } else{
+      victoryText.style.display = 'none'
+    }
+
+
+  }
 
   codeSubmit.addEventListener('click', function(e){
     e.preventDefault();
@@ -20,5 +46,7 @@ document.addEventListener('DOMContentLoaded', function(e){
     // send code to test in ferretLand
     testFerretLand(lineOfCode.value)
     lineOfCode.value = ''
+
+    checkForVictory();
   })
 })
