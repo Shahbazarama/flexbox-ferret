006a79cc0cb6025a95542ea11e1b08f6778906e3
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-31 13:44:38 -0700

    more small organization

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(e){
   let codeSubmit = document.querySelector('#codeSubmit')
   let lineOfCode = document.querySelector('#lineOfCode')
+
+  let ferretImage = document.querySelector('#ferretImage')
+  let rightSide = document.querySelector('.rightSide')
+  function testFerretLand(codeToTest){
+    console.log('hit')
+    rightSide.setAttribute("style", codeToTest)
+  }
+
+
   codeSubmit.addEventListener('click', function(e){
     e.preventDefault();
+
+    // send code to test in ferretLand
+    testFerretLand(lineOfCode.value)
     lineOfCode.value = ''
   })
 })
